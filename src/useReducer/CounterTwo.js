bdc6325cead66b849969cbd67e62ef31f56d35db
Sakil <mdsakiluzzaman@gmail.com>
2022-03-13 19:12:30 +0600

    counter 2 added

--- src/useReducer/CounterTwo.js
+++ src/useReducer/CounterTwo.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { useReducer } from 'react';
 
 
-const initialState = 0
+const initialState = {
+    firstCounter:0,
+    secondCounter:15
+}
 const reducer = (state,action) =>{
-    switch(action){
+    switch(action.type){
         case 'increment':
-            return state+1
+            return {...state,firstCounter:state.firstCounter +action.value}
         case 'decrement':
-            return state-1;
+            return {...state,firstCounter:state.firstCounter -action.value}
+        case 'increment2':
+            return {...state,secondCounter:state.secondCounter +action.value}
+        case 'decrement2':
+            return {...state,secondCounter:state.secondCounter -action.value}
         case 'reset':
-            return initialState;
+            return initialState
         default:
-            return state;
+            return state
     }
    
 
@@ -26,10 +33,17 @@ function CounterTwo() {
      
 
     <div>
-        <div>Count - {count}</div>
-        <button onClick={()=>dispatch('increment')}>Increment</button>
-        <button onClick={()=>dispatch('decrement')}>Decrement</button>
-        <button onClick={()=>dispatch('reset')}>Reset</button>
+        <div>First Counter - {count.firstCounter}</div>
+        <div>Second Counter - {count.secondCounter}</div>
+        <button onClick={()=>dispatch({type:'increment',value:1})}>Increment</button>
+        <button onClick={()=>dispatch({type:'decrement',value:1})}>Decrement</button>
+        <button onClick={()=>dispatch({type:'increment',value:5})}>Increment 5</button>
+        <button onClick={()=>dispatch({type:'decrement',value:5})}>Decrement 5</button>
+        <div>
+            <button onClick={()=>dispatch({type:'increment2',value:1})}>Increment Counter 2</button>
+            <button onClick={()=>dispatch({type:'decrement2',value:1})}>Decrement Counter 2</button>
+        </div>
+        <button onClick={()=>dispatch({type:'reset'})}>Reset</button>
     </div>
   )
 }
