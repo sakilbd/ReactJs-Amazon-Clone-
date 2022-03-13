8d5d8410719eede609449b168bd7f0884c09e034
Sakil <mdsakiluzzaman@gmail.com>
2022-03-13 18:30:39 +0600

    reducer hook example added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import CounterOne from './useReducer/useReducerDemo';
+import CounterTwo from './useReducer/CounterTwo';
+
 
 function App() {
   return (
@@ -12,8 +15,12 @@ function App() {
       <Header/>
         <Switch>
           <Route path="/checkout">
-         
+
             <Checkout/>
+          </Route>
+          <Route path="/counter">
+            <CounterTwo/>
+            <CounterOne/>
           </Route>
           <Route path="/">
           
